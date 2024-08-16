962093d3834898ae4847e55c8db6d1bc5ad0a269
AbdulMoiz <pc16786abdulmoiz@gmail.com>
2024-08-16 14:55:52 +0500

    sidebar , Navbar and other UI snippets

---
+++ comp/nav.js
@@ -0,0 +1,21 @@
+document.getElementById('toggleButton').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    const navbar = document.getElementById('navbar');
+    const content = document.getElementById('content');
+    const toggleButton = document.getElementById('toggleButton');
+    
+    sidebar.classList.toggle('visible');
+    
+    if (sidebar.classList.contains('visible')) {
+        content.style.marginLeft = '250px';
+        navbar.style.marginLeft = '250px';
+    } else {
+        content.style.marginLeft = '0';
+        navbar.style.marginLeft = '2rem';
+    }
+    
+    const barsIcon = toggleButton.querySelector('.fa-bars');
+    const closeIcon = toggleButton.querySelector('.fa-close');
+    barsIcon.style.display = sidebar.classList.contains('visible') ? 'none' : 'inline';
+    closeIcon.style.display = sidebar.classList.contains('visible') ? 'inline' : 'none';
+});
